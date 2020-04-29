8d7edf7875503e48abfd5eddb1e2893103d7e179
dre <908612508@qq.com>
2020-04-30 00:23:19 +0800

    [modify]在windows下暂时以单进程启动，方便开发查看log输出

--- core/app/mini_app_main_delegate.cc
+++ core/app/mini_app_main_delegate.cc
@@ -1,7 +1,9 @@
 #include "mini_app/core/app/mini_app_main_delegate.h"
 
+#include "base/command_line.h"
 #include "base/logging.h"
 #include "base/path_service.h"
+#include "content/public/common/content_switches.h"
 #include "mini_app/core/browser/mini_app_content_browser_client.h"
 #include "mini_app/core/common/mini_app_content_client.h"
 #include "mini_app/core/renderer/mini_app_content_renderer_client.h"
@@ -18,6 +20,12 @@ MiniAppMainDelegate::~MiniAppMainDelegate() {
 }
 
 bool MiniAppMainDelegate::BasicStartupComplete(int* exit_code) {
+#if defined(OS_WIN)
+    //为了看log 所以以进程的方式启动
+    base::CommandLine* line = base::CommandLine::ForCurrentProcess();
+    line->AppendSwitch(switches::kSingleProcess);
+#endif
+
     *exit_code = 0;
     content_client_.reset(new MiniAppContentClient);
     SetContentClient(content_client_.get());
@@ -26,6 +34,10 @@ bool MiniAppMainDelegate::BasicStartupComplete(int* exit_code) {
 
 void MiniAppMainDelegate::PreSandboxStartup() {
     InitializeResourceBundle();
+
+    logging::LoggingSettings log_settings;
+    log_settings.logging_dest = logging::LOG_TO_SYSTEM_DEBUG_LOG | logging::LOG_TO_STDERR;
+    logging::InitLogging(log_settings);
 }
 
 void MiniAppMainDelegate::SandboxInitialized(const std::string& process_type) {
