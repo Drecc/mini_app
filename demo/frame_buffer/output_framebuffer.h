2fe26f92ee313e5eed601450edbe78f0201c3163
Dre <908612508@qq.com>
2020-05-14 15:27:01 +0800

    [bugfix]修复移动目录导致的不能编译以及整理了相关的framebuffer的代码

--- demo/frame_buffer/output_framebuffer.h
+++ demo/frame_buffer/output_framebuffer.h
@@ -1,4 +1,5 @@
 #include <memory>
+#include <linux/fb.h>
 
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "third_party/skia/include/core/SkCanvas.h"
@@ -10,6 +11,7 @@ namespace mini_app {
 
 class Output {
     virtual SkCanvas * Canvas() = 0;
+    virtual std::string ToString() = 0;
 };
 
 class OutputFrameBuffer : public Output {
@@ -17,10 +19,23 @@ public:
     OutputFrameBuffer();
     virtual ~OutputFrameBuffer();
 
+    //Output
     SkCanvas * Canvas() override;
+    std::string ToString() override;
+
+    bool Init(int width, int height);
 
 private:
-    void* frameBuffer_;
+    void* frame_buffer_ = nullptr;
+    void* frame_buffer_1_ = nullptr;
+    void* frame_buffer_2_ = nullptr;
+    int frame_buffer_fd_ = -1;
+    int width_ = 0;
+    int height_ = 0;
+    int screen_size_ = 0;   //width * height * 4
+    //framebuffer的相关信息
+    fb_fix_screeninfo finfo_;
+    fb_var_screeninfo vinfo_;
 
 };
 
