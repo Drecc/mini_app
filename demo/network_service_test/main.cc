c5da266ad7472f0653732d3656bbcddf9faeccf4
Dre <908612508@qq.com>
2020-08-26 14:25:26 +0800

    [feature]增加networkservice使用的demo

---
+++ demo/network_service_test/main.cc
@@ -0,0 +1,189 @@
+#include "base/at_exit.h"
+#include "base/bind.h"
+#include "base/command_line.h"
+#include "base/logging.h"
+#include "base/macros.h"
+#include "base/message_loop/message_loop.h"
+#include "base/run_loop.h"
+#include "base/task/post_task.h"
+#include "base/task/thread_pool/thread_pool_instance.h"
+#include "base/threading/thread.h"
+#include "base/files/file.h"
+#include "base/files/file_path.h"
+
+//mojo
+#include "mojo/core/embedder/embedder.h"
+#include "mojo/core/embedder/scoped_ipc_support.h"
+#include "mojo/public/cpp/bindings/remote.h"
+
+//network service
+#include "services/network/network_service.h"
+#include "services/network/public/mojom/network_service.mojom.h"
+#include "services/network/public/mojom/network_context.mojom.h"
+#include "services/network/public/cpp/url_loader_completion_status.h"
+#include "services/network/public/mojom/url_loader.mojom.h"
+#include "services/network/public/mojom/url_loader_factory.mojom.h"
+#include "services/network/public/mojom/url_response_head.mojom-forward.h"
+
+class MainEnv {
+public:
+  explicit MainEnv(int argc, char** argv) {
+    base::CommandLine::Init(argc, argv);
+    logging::SetLogItems(true, true, true, false);
+    base::ThreadPoolInstance::CreateAndStartWithDefaultParams("network_service_test");
+  }
+  ~MainEnv() = default;
+private:
+  base::AtExitManager at_exit_;
+  base::MessageLoop message_loop_;
+};
+
+class MoJoSupport {
+public:
+  explicit MoJoSupport() : mojo_thread_("mojo") {
+    mojo::core::Init();
+    mojo_thread_.StartWithOptions(
+        base::Thread::Options(base::MessagePumpType::IO, 0));
+    ipc_support_ = std::make_unique<mojo::core::ScopedIPCSupport>(
+        mojo_thread_.task_runner(),
+        mojo::core::ScopedIPCSupport::ShutdownPolicy::CLEAN);
+    
+  }
+  ~MoJoSupport() = default;
+private:
+  base::Thread mojo_thread_;
+  std::unique_ptr<mojo::core::ScopedIPCSupport> ipc_support_;
+};
+
+class NetWorkService {
+public:
+  NetWorkService() : thread_("NetWorkService") {
+    base::Thread::Options options(base::MessagePumpType::IO, 0);
+    thread_.StartWithOptions(options);
+  }
+  ~NetWorkService() = default;
+
+  void Init(mojo::PendingReceiver<network::mojom::NetworkService> receiver) {
+    thread_.task_runner()->PostTask(FROM_HERE, base::BindOnce(&NetWorkService::CreateNetWorkService, base::Unretained(this), std::move(receiver)));
+  }
+
+private:
+  void CreateNetWorkService(mojo::PendingReceiver<network::mojom::NetworkService> receiver) {
+    if(!network_service_)
+      network_service_ = network::NetworkService::Create(std::move(receiver));
+  }
+  std::unique_ptr<network::NetworkService> network_service_;
+  base::Thread thread_;
+};
+
+class DemoUrlLoaderClient : public network::mojom::URLLoaderClient {
+public:
+  void OnReceiveResponse(network::mojom::URLResponseHeadPtr response_head) override {
+    LOG(INFO) << "OnReceiveResponse";
+  }
+
+  void OnReceiveRedirect(const net::RedirectInfo& redirect_info,
+                         network::mojom::URLResponseHeadPtr response_head) override {
+    LOG(INFO) << "OnReceiveRedirect " << redirect_info.new_url;
+  }
+
+  void OnReceiveCachedMetadata(mojo_base::BigBuffer data) override {
+    LOG(INFO) << "OnReceiveCachedMetadata";
+  }
+
+  void OnTransferSizeUpdated(int32_t transfer_size_diff) override {
+    LOG(INFO) << "OnTransferSizeUpdated";
+  }
+
+  void OnUploadProgress(int64_t current_position,
+                        int64_t total_size,
+                        OnUploadProgressCallback ack_callback) override {
+    LOG(INFO) << "OnUploadProgress";
+  }
+
+  void OnStartLoadingResponseBody(
+      mojo::ScopedDataPipeConsumerHandle body) override {
+    LOG(INFO) << "OnStartLoadingResponseBody";
+  }
+
+  void OnComplete(const network::URLLoaderCompletionStatus& status) override {
+    LOG(INFO) << "OnComplete " << status.error_code;
+  }
+
+  mojo::PendingRemote<network::mojom::URLLoaderClient> CreateRemote() {
+    mojo::PendingRemote<network::mojom::URLLoaderClient> client_remote;
+    receiver_.Bind(client_remote.InitWithNewPipeAndPassReceiver());
+    return client_remote;
+  }
+
+private:
+  mojo::Receiver<network::mojom::URLLoaderClient> receiver_{this};
+};
+
+class DemoURLLoader {
+
+
+
+};
+
+int RunMain() {
+  base::RunLoop run_loop;
+
+  mojo::Remote<network::mojom::NetworkService> remote_service_;
+
+  NetWorkService service;
+  service.Init(remote_service_.BindNewPipeAndPassReceiver());
+
+  remote_service_.QueryVersion(base::BindRepeating(
+      [](uint32_t value) {
+        LOG(INFO) << "create NetworkService success ";
+      }));
+
+  base::FilePath log_path("log_network");
+  base::File file(log_path, base::File::FLAG_CREATE_ALWAYS | base::File::FLAG_WRITE);
+  if (!file.IsValid()) {
+    LOG(ERROR) << "Failed opening NetLog: " << log_path.value();
+  } else {
+    remote_service_->StartNetLog(std::move(file), net::NetLogCaptureMode::kEverything, base::DictionaryValue());
+  }
+
+  mojo::Remote<network::mojom::NetworkContext> network_context_;
+  network::mojom::NetworkContextParamsPtr context_params =
+        network::mojom::NetworkContextParams::New();
+  remote_service_->CreateNetworkContext(
+        network_context_.BindNewPipeAndPassReceiver(),
+        std::move(context_params));
+
+  std::unique_ptr<DemoUrlLoaderClient> client_;
+  mojo::Remote<network::mojom::URLLoader> loader_;
+  client_.reset(new DemoUrlLoaderClient());
+
+  mojo::Remote<network::mojom::URLLoaderFactory> loader_factory;
+  network::mojom::URLLoaderFactoryParamsPtr params =
+      network::mojom::URLLoaderFactoryParams::New();
+  params->process_id = 0;
+  params->is_corb_enabled = false;
+  network_context_->CreateURLLoaderFactory(
+      loader_factory.BindNewPipeAndPassReceiver(), std::move(params));
+
+  loader_.reset();
+  network::ResourceRequest request;
+  request.url = GURL("http://www.google.com");
+  request.method = "GET";
+  request.request_initiator = url::Origin();
+  loader_factory->CreateLoaderAndStart(
+      loader_.BindNewPipeAndPassReceiver(), 1, 1, network::mojom::kURLLoadOptionNone, request,
+      client_->CreateRemote(),
+      net::MutableNetworkTrafficAnnotationTag(net::DefineNetworkTrafficAnnotation("test", "Traffic annotation")));
+
+  run_loop.Run();
+  return 0;
+}
+
+int main(int argc, char** argv) {
+  MainEnv env(argc, argv);
+  MoJoSupport mojo_support;
+  LOG(INFO) << "start network service test demo...";
+  return RunMain();
+}
+
